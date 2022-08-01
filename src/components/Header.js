c9b66985a41ad5f41bdd75eb02b0a748a97ecdf3
Alvin Accad <alvinaccad5@gmail.com>
2022-08-01 16:41:06 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React, { Component, useState } from 'react'
+// import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const Header = (args) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...args} className="navbar-expand">
+        <NavbarBrand href="/">Friskey Feline</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/catshow">Show</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/catnew">Edit</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+
+export default Header
---
+++ src/pages/CatEdit.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class CatEdit extends Component {
+  render() {
+    return (
+      <div>CatEdit</div>
+    )
+  }
+}
+
+export default CatEdit
